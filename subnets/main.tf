70260d8b0852f2c37edfd18d9c6089ea01f26a3f
vivek <vivekemails17@gmail.com>
2023-12-02 00:37:10 +0530

    route entry

--- subnets/main.tf
+++ subnets/main.tf
@@ -7,3 +7,12 @@ resource "aws_subnet" "main" {
     Name = each.key
   }
 }
+
+resource "aws_route_table" "main" {
+  vpc_id   = var.vpc_id
+  for_each = var.subnets
+
+  tags = {
+    Name = "each.key"
+  }
+}
