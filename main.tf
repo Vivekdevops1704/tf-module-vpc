823d604baf27de4485a85546dee0d19f4538aa47
vivek <vivekemails17@gmail.com>
2023-12-02 17:26:20 +0530

    adding files

--- main.tf
+++ main.tf
@@ -2,10 +2,10 @@ resource "aws_vpc" "main" {
   cidr_block       =   var.cidr
 }
 module "subnets" {
- source = "./subnets"
+ source   = "./subnets"
  for_each = var.subnets
- subnets = each.value
- vpc_id = aws_vpc.main.id
+ subnets  = each.value
+ vpc_id   = aws_vpc.main.id
 }
 resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.main.id
@@ -14,20 +14,33 @@ resource "aws_internet_gateway" "igw" {
   }
 }
 resource "aws_route" "igw" {
-  //for_each =  lookup(lookup(module.subnets,"public",null),"route_table_id",null)
-  count  = local.private_route_table_ids
-  route_table_id            = element(local.private_route_table_ids, count.index)
+  for_each                  = lookup(lookup(module.subnets,"public",null),"route_table_id",null)
+  route_table_id            = each.value[id]
   destination_cidr_block    = "0.0.0.0/0"
-  nat_gateway_id = element(aws_nat_gateway.ngw.*.id, count.index)
+  gateway_id                = aws_internet_gateway.igw.id
 }
 resource "aws_nat_gateway" "ngw" {
   //for_each =  lookup(lookup(module.subnets,"public",null),"subnet_ids",null)
-  count = local.public_subnet_ids
+  count           = length(local.public_subnet_ids)
   //allocation_id = lookup(lookup(aws_eip.ngw,each.key,null),"id",null)
-  allocation_id = element(aws_eip.ngw.*.id, count.index)
-  subnet_id =   element(local.public_subnet_ids, count.index)
+  allocation_id   = element(aws_eip.ngw.*.id, count.index)
+  subnet_id       = element(local.public_subnet_ids, count.index)
+}
+resource "aws_route" "ngw" {
+  //for_each =  lookup(lookup(module.subnets,"public",null),"route_table_id",null)
+  count  = length(local.private_route_table_ids)
+  route_table_id            = element(local.private_route_table_ids, count.index)
+  destination_cidr_block    = "0.0.0.0/0"
+  nat_gateway_id            = element(aws_nat_gateway.ngw.*.id, count.index)
 }
 resource "aws_eip" "ngw" {
-  for_each =  lookup(lookup(module.subnets,"public",null),"route_table_id",null)
+  //for_each =  lookup(lookup(module.subnets,"public",null),"route_table_id",null)
+  count    =  length(local.public_subnet_ids)
   domain   = "vpc"
+}
+
+resource "aws_vpc_peering_connection" "peering" {
+  peer_vpc_id   = aws_vpc.main.id
+  vpc_id        = var.aws_default_vpc.id
+  auto_accept   = true
 }
--- outputs.tf
+++ outputs.tf
@@ -5,3 +5,6 @@ output "subnets" {
 output "vpc_id" {
   value = aws_vpc.main.id
 }
+ output "public_subnet_id" {
+  value = local.app_subnet_ids
+ }
